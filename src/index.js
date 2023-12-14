db003ad6a51b5d6694e21f04354dcc754f59c112
Oleksandr Lisovich <lisovyhs@gmail.com>
2023-12-14 07:32:32 +0200

    update-browserslist-db@latest

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	< BrowserRouter basename={process.env.PUBLIC_URL} >
+	< HashRouter >
 		{/* <React.StrictMode> */}
 		<Provider store={store}>
 			<App />
 		</Provider>
 		{/* </React.StrictMode> */}
-	</BrowserRouter >);
+	</ HashRouter >);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
